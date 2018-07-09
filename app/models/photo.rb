c65ca1f9b45d6ab64bc02c1089af9105ed9981c9
Aris Nance <nance.aris@gmail.com>
2018-07-09 02:31:13 +0000

    UpdatedRails

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,13 +8,17 @@ class Photo < ActiveRecord::Base
     belongs_to :user
     
     # Tagging system
-    def self.tagged_with(name)
-      Tag.find_by_name!(name).photos
-    end
-    
     def self.tag_counts
-      Tag.select("tags.*, count(taggings.tag_id) as count").
-        joins(:taggings).group("taggings.tag_id")
+      Tag.select("tags.id, tags.name,count(taggings.tag_id) as count").
+      joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
+    end 
+    
+    def tag_cloud(tags, classes)
+        max = tags.sort_by(&:count).last
+        tags.each do |tag|
+        index = tag.count.to_f / Integer(max.count) * (classes.size - 1)
+        yield(tag, classes[index.round])
+    end
     end
     
     def tag_list
