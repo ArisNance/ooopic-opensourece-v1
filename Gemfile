9294eb5fde6e59f9a5620956888566f6944938e9
Aris Nance <nance.aris@gmail.com>
2018-07-09 01:56:16 +0000

    RemovedSass

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.5'
 
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+#gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,11 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  
+  config.assets.configure do |env|
+    env.cache = ActiveSupport::Cache.lookup_store(:null_store)
+  end
+  config.assets.compile = false
 
   # Do not eager load code on boot.
   config.eager_load = false
