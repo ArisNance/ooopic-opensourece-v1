5502f340f10b499f27fec8ed1373aa30ed784df4
Aris Nance <nance.aris@gmail.com>
2018-07-09 22:21:04 +0000

    AddAdminToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708025825) do
+ActiveRecord::Schema.define(version: 20180709222019) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -71,8 +71,9 @@ ActiveRecord::Schema.define(version: 20180708025825) do
     t.string   "image"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "admin",            default: false
   end
 
 end
